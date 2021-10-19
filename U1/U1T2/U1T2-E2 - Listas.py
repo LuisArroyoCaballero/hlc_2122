b3ed965df4e8cb8028452630de58730b9b2922b6
LuisArroyoCaballero <alum.larroyoc.2020@iesalixar.org>
2021-10-19 20:06:48 +0200

    Prueba Cadenas y listas

--- U1/U1T2/U1T2-E2 - Listas.py
+++ U1/U1T2/U1T2-E2 - Listas.py
@@ -10,20 +10,17 @@
 
 lista1 = ["M", "nom", "e", "Che", "jajajajaj"]
 lista2 = ["i", "bre", "s", "ma"]
+lista3 = ""
 
 
 if len(lista1) < len(lista2):
-    for i in range(len(lista1)):
+    for i in range(len(lista1)-1):
         lista3 = lista3+lista1[i]+lista2[i]+" "
-        cont+=1
-    lista3 = lista3+(lista2[cont:])
-elif len(lista1) > len(lista2):
+    lista3.concat(lista2[len(lista1)-1:])
+else:
     for i in range(len(lista2)):
         lista3 = lista3+lista1[i]+lista2[i]+" "
-        cont+=1
-    lista3 = lista3+(lista1[cont:])
-else:
-    for i in range(len(lista1)):
-        lista3 = lista3+lista1[i]+lista2[i]+" "
+    lista3.concat(lista1[len(lista2)-1:])
+
 
 print(lista3)
